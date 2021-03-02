30c64d77bb6d2b17e4b9e16833d1d963074e33e4
DJWOMS <socanime@gmail.com>
2021-03-02 17:09:29 +0300

    change app video

---
+++ video/models.py
@@ -0,0 +1,19 @@
+import datetime
+import ormar
+from typing import Optional, Union
+from db import MainMata
+
+from user.models import User
+
+
+class Video(ormar.Model):
+    class Meta(MainMata):
+        pass
+
+    id: int = ormar.Integer(primary_key=True)
+    title: str = ormar.String(max_length=50)
+    description: str = ormar.String(max_length=500)
+    file: str = ormar.String(max_length=1000)
+    create_at: datetime.datetime = ormar.DateTime(default=datetime.datetime.now)
+    user: Optional[User, int] = ormar.ForeignKey(User)
+
---
+++ video/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+from user.schemas import User
+
+
+class UploadVideo(BaseModel):
+    title: str
+    description: str
+
+
+class GetListVideo(BaseModel):
+    id: int
+    title: str
+    description: str
+
+
+class GetVideo(GetListVideo):
+    user: User
+
+
+class Message(BaseModel):
+    message: str
