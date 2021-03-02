9a9c8b092a785110190f75f5bb071ad58fa65969
DJWOMS <socanime@gmail.com>
2021-03-02 17:44:44 +0300

    Fixed model in user app

--- user/models.py
+++ user/models.py
@@ -1,8 +1,9 @@
 import ormar
 
-from fastapi_users.db import OrmarBaseUserModel
+from fastapi_users.db import OrmarBaseUserModel, OrmarUserDatabase
 
 from db import MainMata
+from user.schemas import UserDB
 
 
 class User(OrmarBaseUserModel):
@@ -10,3 +11,6 @@ class User(OrmarBaseUserModel):
         pass
 
     username: str = ormar.String(max_length=100, unique=True)
+
+
+user_db = OrmarUserDatabase(UserDB, User)
